f2bcec94bc4263489c2cbaab5e8429ef4c5e0c85
probobbo <probobbo@gmail.com>
2016-11-08 18:16:47 +0100

    Aggiunta la possibilità di disegnare con il mouse

---
+++ Assets/MouseSpawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseSpawn : MonoBehaviour {
+
+    public GameObject scratch;
+    Vector3 mouse;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0)) 
+        {
+            mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(scratch, new Vector3(mouse.x, mouse.y, 3), Quaternion.identity);
+        }
+	}
+}
