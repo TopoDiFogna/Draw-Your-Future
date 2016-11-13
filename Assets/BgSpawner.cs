83a3c7637eb079457c870ad6a6404c79ec640e65
probobbo <probobbo@gmail.com>
2016-11-13 14:07:51 +0100

    Spawn di oggetti e piattaforme (versione estremamente parziale)

---
+++ Assets/BgSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgSpawner : MonoBehaviour {
+
+    public GameObject Obj;
+    bool en = false;
+	
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        print("Triggered");
+        if(coll.gameObject.tag == "Scratch" && !en)
+        {
+            print("Triggered inside");
+            Instantiate(Obj, transform.position, Quaternion.identity);
+            en = true;
+            StartCoroutine(Reset(10));
+        }
+    }
+
+    IEnumerator Reset(int time)
+    {
+        yield return new WaitForSeconds(time);
+        en = false;
+    }
+}
