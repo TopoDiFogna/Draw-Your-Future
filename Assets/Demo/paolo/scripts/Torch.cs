fa5a4ef01b89e4c7d14fef5266d52dac3683e485
Paolo Paterna <paolo.paterna@gmail.com>
2016-11-27 19:37:50 +0100

    Added Paolo prefabs and dependencies
    *Cage
    *Torch
    *Bomb
    *Jumping Platform

---
+++ Assets/Demo/paolo/scripts/Torch.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Torch : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+    public Sprite m_on_status;
+    public Sprite m_off_status;
+
+    // Use this for initialization
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Scratch")
+        {
+            print("entering");
+            sr.sprite = m_on_status;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Scratch")
+        {
+            print("exiting");
+             sr.sprite = m_off_status;
+        }
+    }
+}
