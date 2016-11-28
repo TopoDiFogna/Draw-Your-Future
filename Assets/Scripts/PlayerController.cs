d62b387b36958aca035ae33b6972752013adc4a4
Paolo Paterna <paolo.paterna@gmail.com>
2016-11-28 11:40:17 +0100

    Added Missing player script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,24 +3,34 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    public int m_scratch_layer = 9;
 
-    int lay;
-    public int ScLayer;
-
-    Transform tr;
-    Rigidbody2D rb;
-    public GameObject paint;
+    public GameObject m_paint;
 
     [Range(0, 100)]
-    public float Speed;
+    public float m_speed = 20f;
+
     [Range(0, 100)]
-    public float ClimbingSpeed;
+    public float m_climbing_speed = 15f;
 
     [Range(0, 200)]
-    public float Jump_force;
+    public float m_Jump_force = 10f;
 
+    // Player Properties
+    int lay;
+    Transform tr;
+    Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer sr;
+
+    // Status variables
     bool jumping = false;
     bool climbing = false;
+    bool facing_right = true;
+
+    //Controls variables
+    float m_horizontal = 0f;
+    float m_vertical = 0f;
 
     // Use this for initialization
     void Start()
@@ -28,25 +38,43 @@ public class PlayerController : MonoBehaviour
         lay = gameObject.layer;
         tr = transform;
         rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        m_horizontal = Input.GetAxisRaw("Horizontal");
+        m_vertical = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", Mathf.Abs(m_horizontal));
+
+        if(facing_right && m_horizontal < 0)
+        {
+            sr.flipX = true;
+            facing_right = false;
+        }
+        else if(!facing_right && m_horizontal > 0)
+        {
+            sr.flipX = false;
+            facing_right = true;
+        }
+
         if (climbing)
         {
-            if (Input.GetAxisRaw("Vertical") != 0){
-                tr.position += new Vector3(0, ClimbingSpeed * Input.GetAxisRaw("Vertical") * Time.deltaTime, 0);
+            if (m_vertical != 0)
+            {
+                tr.position += new Vector3(0, m_climbing_speed * m_vertical * Time.deltaTime, 0);
             }
         }
         else
         {
-            //TODO 
+            // TODO 
         }
  
         if (Input.GetKeyDown(KeyCode.W) && !jumping)
         {
-            rb.AddForce(Vector2.up * Jump_force, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * m_Jump_force, ForceMode2D.Impulse);
             jumping = true;
         }
     }
@@ -55,32 +83,29 @@ public class PlayerController : MonoBehaviour
     {
         if (!climbing)
         {
-            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, rb.velocity.y);
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * m_speed, rb.velocity.y);
         }
     }
 
-
-
-
-
-
-    
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (jumping == true && coll.gameObject.tag == "Terrain")
         {
             jumping = false;
         }
-
-
     }
 
+
+    void onCollisionExit2D(Collision2D coll)
+    {
+
+    }
 
     void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Scratch")
         {
-            gameObject.layer = ScLayer;
+            gameObject.layer = m_scratch_layer;
         }
 
         if (coll.gameObject.tag == "Climbable")
@@ -95,7 +120,7 @@ public class PlayerController : MonoBehaviour
     {
         if (coll.gameObject.tag == "Scratch")
         {
-            gameObject.layer = ScLayer;
+            gameObject.layer = m_scratch_layer;
         }
     }
 
