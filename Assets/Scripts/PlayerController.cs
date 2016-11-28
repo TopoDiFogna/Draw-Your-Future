9b6f4b32fdee4ea9089e0174d3468ec5e9950d49
probobbo <probobbo@gmail.com>
2016-11-28 17:34:31 +0100

    Integrato PlayerControll con PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,7 +25,9 @@ public class PlayerController : MonoBehaviour
 
     // Status variables
     bool jumping = false;
-    bool climbing = false;
+
+    public bool IsNearLadder = false;
+
     bool facing_right = true;
 
     //Controls variables
@@ -60,7 +62,7 @@ public class PlayerController : MonoBehaviour
             facing_right = true;
         }
 
-        if (climbing)
+        /*if (climbing)
         {
             if (m_vertical != 0)
             {
@@ -70,9 +72,9 @@ public class PlayerController : MonoBehaviour
         else
         {
             // TODO 
-        }
+        }*/
  
-        if (Input.GetKeyDown(KeyCode.W) && !jumping)
+        if (Input.GetKeyDown(KeyCode.W) && !jumping && !IsNearLadder)
         {
             rb.AddForce(Vector2.up * m_Jump_force, ForceMode2D.Impulse);
             jumping = true;
@@ -81,9 +83,13 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (!climbing)
+        if (!IsNearLadder)
         {
             rb.velocity = new Vector2(Input.GetAxis("Horizontal") * m_speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * m_speed, Input.GetAxis("Vertical") * m_climbing_speed);
         }
     }
 
@@ -111,7 +117,7 @@ public class PlayerController : MonoBehaviour
         if (coll.gameObject.tag == "Climbable")
         {
             jumping = false;
-            climbing = true;
+            IsNearLadder = true;
             rb.isKinematic = true;
         }
     }
@@ -133,8 +139,8 @@ public class PlayerController : MonoBehaviour
 
         if (coll.gameObject.tag == "Climbable")
         {
-            climbing = false;
-            rb.isKinematic = true;
+            IsNearLadder = false;
+            rb.isKinematic = false;
         }
     }
 }
