e20aa2088fde75b9c27dbeb14c7a3983612fb946
probobbo <probobbo@gmail.com>
2016-11-29 17:33:07 +0100

    Aggiunto di nuovo il rimpicciolimento dell'area dei paint

--- Assets/Scripts/Paint.cs
+++ Assets/Scripts/Paint.cs
@@ -21,34 +21,31 @@ public class Paint : MonoBehaviour {
     {
         timer = 0f;
         vanishing = false;
-        StartCoroutine(DisablePaint());
     }
 
 	// Update is called once per frame
 	void Update () {
 
-        //timer += Time.deltaTime;
-        //if (timer >= timetolive && !vanishing)
-        //{
-        //    timer = 0f;
-        //    vanishing = true;
-        //    //gameObject.SetActive(false);
-        //}
-        //if (vanishing)
-        //{
-        //    tr.localScale -= new Vector3(0.01f, 0.01f, 0);
-        //    if (tr.localScale.x <= 0)
-        //    {
-        //        tr.localScale = scale;
-        //        coll.enabled = false;
-        //        gameObject.SetActive(false);
-        //    }
-        //}
-	}
+        timer += Time.deltaTime;
+        if (timer >= timetolive && !vanishing)
+        {
+            timer = 0f;
+            vanishing = true;
+            //gameObject.SetActive(false);
+        }
+        if (vanishing)
+        {
+            tr.localScale -= new Vector3(0.01f, 0.01f, 0);
+            if (tr.localScale.x <= 0)
+            {
+                StartCoroutine(DisablePaint());
+                tr.localScale = scale;
+            }
+        }
+    }
 
     IEnumerator DisablePaint()
     {
-        yield return new WaitForSeconds(timetolive);
         tr.position = new Vector2(1000,1000);
         yield return new WaitForSeconds(0.2f);
         gameObject.SetActive(false);
