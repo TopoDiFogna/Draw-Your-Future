ea849b6331a2ed4ebc27abb69d76dd1e3b2d8543
probobbo <probobbo@gmail.com>
2016-12-01 17:59:07 +0100

    Il muro ora si rompe in 3 hit (numero modificabile da editor) manca ancora un feedback visivo

--- Assets/DestructibleObject.cs
+++ Assets/DestructibleObject.cs
@@ -3,11 +3,21 @@ using System.Collections;
 
 public class DestructibleObject : MonoBehaviour {
 
+    int m_counter;
+    public int m_hitlimit;
+
+    void Start()
+    {
+        m_counter = 0;
+    }
+
 	void OnCollisionEnter2D(Collision2D coll)
     {
         if(coll.gameObject.tag == "Projectile")
         {
-            Destroy(gameObject);
+            m_counter++;
+            if(m_counter>=m_hitlimit)
+                Destroy(gameObject);
         }
     }
 }
