4a2eba7e3d80ce8babea26337c227908cba98b10
probobbo <probobbo@gmail.com>
2016-12-01 17:34:28 +0100

    Aggiunto muro rompibile e tag projectile alla palla di cannone

---
+++ Assets/DestructibleObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructibleObject : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "Projectile")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
