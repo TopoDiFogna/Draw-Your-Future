bfb2287b5fa02a1e183ce128d6fea5dda8dd38c6
probobbo <probobbo@gmail.com>
2016-12-05 18:00:01 +0100

    Menu principale aggiunto

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject P_butt, P_cred;
+
+	public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void CreditsOn()
+    {
+        P_butt.SetActive(false);
+        P_cred.SetActive(true);
+    }
+
+    public void CreditsOff()
+    {
+        P_butt.SetActive(true);
+        P_cred.SetActive(false);
+    }
+}
