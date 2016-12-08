786a609018c174fa9bad196b465e6b8832fff5f9
Paolo Paterna <paolo.paterna@gmail.com>
2016-12-08 18:01:11 +0100

    Made Swinging platform

---
+++ Assets/Scripts/MakeChild.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeChild : MonoBehaviour {
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        collision.transform.rotation = gameObject.GetComponentInParent<Transform>().rotation;
+        collision.transform.parent = transform;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.transform.rotation = Quaternion.identity;
+        collision.transform.parent = null;
+    }
+}
---
+++ Assets/Scripts/Objects/SwingingObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingingObject : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public float maxRotation = 30.0f;
+
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, maxRotation * Mathf.Sin(Time.time * speed));
+    }
+}
