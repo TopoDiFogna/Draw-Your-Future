91c10da66586fa1e6ba4f9fc3ff93c3795ea1bd6
probobbo <probobbo@gmail.com>
2016-12-09 12:00:58 +0100

    Fixate le nuvole

--- Assets/Demo/Erica/Scripts/LightningController.cs
+++ Assets/Demo/Erica/Scripts/LightningController.cs
@@ -3,41 +3,24 @@ using System.Collections;
 
 public class LightningController : MonoBehaviour {
 
-    SpriteRenderer rend;
-    Collider2D coll;
-    bool visible = false;
     public float timeVisible;
-    public float timeNotVisible;
 
 	// Use this for initialization
 	void Start () {
-        rend = GetComponent<SpriteRenderer>();
-        coll = GetComponent<BoxCollider2D>();
-	}
+        
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine("Lightning");
+    }
 	
 	// Update is called once per frame
-	void Update () {
-        if (!visible)
-        {
-            StartCoroutine("Lightning");
-        }
-	}
 
     IEnumerator Lightning()
     {
-        visible = true;
-        rend.enabled = true;
-        coll.enabled = true;
+       
         yield return new WaitForSeconds(timeVisible);
-        rend.enabled = false;
-        coll.enabled = false;
-        yield return new WaitForSeconds(timeNotVisible);
-        visible = false;
-    }
-
-
-    void OnTriggerEnter2D(Collider2D coll)
-    {
-        //TODO PLAYER DIES
+        gameObject.SetActive(false);
     }
 }
