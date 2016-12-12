77e30d0b94657c8b1a98a7fb7577cc1cba650969
probobbo <probobbo@gmail.com>
2016-12-12 20:32:39 +0100

    Aggiunto singleton alla colonna sonora e fulvio nel credits

--- Assets/Scripts/BackgroundSound.cs
+++ Assets/Scripts/BackgroundSound.cs
@@ -3,8 +3,18 @@ using System.Collections;
 
 public class BackgroundSound : MonoBehaviour {
 
+    private static volatile BackgroundSound istance;
+
 	// Use this for initialization
 	void Start () {
+        if (istance == null)
+        {
+            istance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         DontDestroyOnLoad(gameObject);
 	}
 	
