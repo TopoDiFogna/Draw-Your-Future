5f4ce2367a5e1183a6ee1ed09db237833b62f641
probobbo <probobbo@gmail.com>
2016-12-20 17:40:14 +0100

    Aggiunti prefab dei tizi che tirano le lance. Agiunte le formiche. Aggiunto il serpente che saltella.

---
+++ Assets/Scripts/NPCs/BouncySnakeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncySnakeController : MonoBehaviour
+{
+
+    public float JumpImpulse = 10;
+    Rigidbody2D rb;
+    bool jumping;
+
+    // Use this for initialization
+    void Start()
+    {
+        jumping = false;
+        rb = GetComponent<Rigidbody2D>();
+        //StartCoroutine(Jump());
+    }
+
+    private IEnumerator Jump()
+    {
+        yield return new WaitForSeconds(1);
+        rb.AddForce(Vector2.up * JumpImpulse, ForceMode2D.Impulse);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Terrain")
+        {
+            StartCoroutine(Jump());
+        }
+    }
+}
