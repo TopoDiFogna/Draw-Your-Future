2a7e5a3026ddc23079fde9de6ab3f7364c22992a
unknown <busghella@gmail.com>
2016-12-28 12:01:03 +0100

    tronco fixato e aggiunta doppia sprite per roccia più grande

--- Assets/Scripts/Objects/Trunk.cs
+++ Assets/Scripts/Objects/Trunk.cs
@@ -4,21 +4,30 @@ using UnityEngine;
 
 public class Trunk : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    bool active = true;
+    public float timeToDeactivate = 0.7f;
+
+    // Use this for initialization
+    private void OnEnable()
+    {
+        active = true;
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Hyppo")
+        if (collision.gameObject.tag == "Terrain"){
+            active = false;
+            StartCoroutine("Deactivate");
+        }
+        if(collision.gameObject.tag == "Hyppo" && active)
         {
             collision.gameObject.SetActive(false);
         }
+    }
+
+    IEnumerator Deactivate()
+    {
+        yield return new WaitForSeconds(timeToDeactivate);
+        gameObject.SetActive(false);
     }
 }
