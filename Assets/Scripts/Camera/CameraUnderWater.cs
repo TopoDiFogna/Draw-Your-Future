0333249e29b50739d70c710205095d05aae318a4
unknown <busghella@gmail.com>
2016-12-31 17:46:22 +0100

    aggiunto script per muovere la camera sott'acqua

---
+++ Assets/Scripts/Camera/CameraUnderWater.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraUnderWater : MonoBehaviour {
+
+    BoxCollider2D coll;
+
+
+	// Use this for initialization
+	void Start () {
+        coll = GetComponent<BoxCollider2D>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ostrich")
+        {
+            CameraController camControl = Camera.main.GetComponent<CameraController>();
+            Debug.Log(camControl.m_minBounds.ToString());
+            Debug.Log(coll.size.y.ToString());
+            camControl.m_minBounds -= new Vector2(0, coll.size.y);
+        }
+
+    }
+}
