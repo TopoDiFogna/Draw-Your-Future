26617066529ca6ebacc149fec3134b1f1e365653
unknown <busghella@gmail.com>
2016-12-31 16:47:51 +0100

    L'albero si anima mentre viene rotto dallo yoyo

--- Assets/Scripts/Objects/DestructibleObject.cs
+++ Assets/Scripts/Objects/DestructibleObject.cs
@@ -5,10 +5,13 @@ public class DestructibleObject : MonoBehaviour {
 
     int m_counter;
     public int m_hitlimit;
+    public Sprite[] breaking_sprites;
+    SpriteRenderer sr;
 
     void Start()
     {
         m_counter = 0;
+        sr = GetComponent<SpriteRenderer>();
     }
 
 	void OnCollisionEnter2D(Collision2D coll)
@@ -18,7 +21,15 @@ public class DestructibleObject : MonoBehaviour {
             m_counter++;
             coll.gameObject.SetActive(false);
             if (m_counter >= m_hitlimit)
+            {
+                //TODO animazione di rottura
                 gameObject.SetActive(false);
+            }
+            else
+            {
+                sr.sprite = breaking_sprites[m_counter];
+            }
+
         }
     }
 }
