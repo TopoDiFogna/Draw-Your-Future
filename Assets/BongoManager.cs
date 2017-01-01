729a5a5adda027703a0290a5e59d6531a5ed20de
Marcello <probobbo@gmail.com>
2017-01-01 17:36:21 +0100

    Finito l'indovinello coi tamburi. Devo perfezionare una parte

--- Assets/BongoManager.cs
+++ Assets/BongoManager.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class BongoManager : MonoBehaviour {
 
+    bool pressed;
+    public BongoPuzzle bp;
+    public int id;
+    AudioSource auso;
+
 	// Use this for initialization
 	void Start () {
-		
+        pressed = false;
+        auso = GetComponent<AudioSource>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Player" && !pressed)
+        {
+            pressed = true;
+            auso.Play(0);
+            bp.AddElement(id);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.tag == "Player")
+        {
+            pressed = false;
+        }
+    }
 }
