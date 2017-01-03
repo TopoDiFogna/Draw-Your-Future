7c7499d007a4a54c791cd0e4036af3876f94d3b9
Paolo Paterna <paolo.paterna@gmail.com>
2017-01-03 15:54:38 +0100

    Added mice behaviour

---
+++ Assets/Scripts/Objects/Cheese.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheese : MonoBehaviour {
+
+    public GameObject m_cheese;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Scratch")
+        {
+            m_cheese.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Scratch")
+        {
+            m_cheese.gameObject.SetActive(false);
+        }
+    }
+}
