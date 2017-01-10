03bca81642db834918486168d3880f0772bbc745
unknown <busghella@gmail.com>
2017-01-10 16:55:34 +0100

    fixato spawner del tronco tiodiomarcellochemirompilecoseilgiornodellapresentazione

--- Assets/Scripts/Objects/YoyoSpawner.cs
+++ Assets/Scripts/Objects/YoyoSpawner.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class YoyoSpawner : MonoBehaviour {
 
-    public GameObject LR_Start;
-    public LineRenderer lr;
+    public GameObject LR_Start = null;
+    public LineRenderer lr = null;
     bool activated = false;
     public GameObject m_spawned_object;
 
     private void Start()
     {
-        lr.SetPosition(0, LR_Start.transform.position);
-        lr.SetPosition(1, LR_Start.transform.position);
-        lr.enabled = false;
+
         m_spawned_object = Instantiate(m_spawned_object);
         m_spawned_object.SetActive(false);
         if(m_spawned_object.GetComponent<Yoyo>() != null)
         {
+            lr.SetPosition(0, LR_Start.transform.position);
+            lr.SetPosition(1, LR_Start.transform.position);
+            lr.enabled = false;
             m_spawned_object.GetComponent<Yoyo>().flamingo_animator = GameObject.FindGameObjectWithTag("Flamingo").GetComponent<Animator>();
             m_spawned_object.GetComponent<Yoyo>().lr = lr;
         }
